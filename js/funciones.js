510545b29762c05e66ab12652f952d21e7e8029c
Rayhsvr <rayhsvr@gmail.com>
2021-05-03 18:49:51 -0500

    Cálculo Nomina básica, versión final

--- js/funciones.js
+++ js/funciones.js
@@ -1,7 +1,7 @@
 function calcularNomina () {
   const salario = parseFloat(document.getElementById('salario').value)
   const dias = parseFloat(document.getElementById('dias').value)
-  if (salario > 0 && dias > 0) {
+  if (salario > 0 && dias >= 0) {
     const importe = salario * dias
     document.getElementById('importe').value = Number.parseFloat(importe)
   }
