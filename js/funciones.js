0ea36fd3db44fb4e910f62ea80a8d6aa588e11dc
Rayhsvr <rayhsvr@gmail.com>
2021-06-03 21:12:18 -0500

    Versión 3 jun 2021

--- js/funciones.js
+++ js/funciones.js
@@ -8,10 +8,10 @@ function calcularNomina () {
     const quincena = salario * dias
     if (tipo[0].checked) {
       bono = salario * 10
-      isr = quincena * .25
+      isr = quincena * 0.25
     } else {
       bono = salario * 5
-      isr = quincena * .15
+      isr = quincena * 0.15
     }
     if (dias < 15) {
       bono = 0
@@ -27,4 +27,29 @@ function calcularNomina () {
     }
     document.getElementById('importe').value = '0'
   }
+}
+
+function generaOptions () {
+  const tipo = document.getElementsByName('tipo')
+  const depto = document.getElementsByName('depto')
+  if (tipo[0].checked) {
+    depto.options[0].value = 'COM'
+    depto.options[0].text = 'Compras'
+    depto.options[1].value = 'DIR'
+    depto.options[1].text = 'Dirección'
+    depto.options[2].value = 'INF'
+    depto.options[2].text = 'Informática'
+    depto.options[3].value = 'PER'
+    depto.options[3].text = 'Personal'
+    depto.options[4].value = 'VEN'
+    depto.options[4].text = 'Ventas'
+  } else {
+    depto.options[0].value = 'MAN'
+    depto.options[0].text = 'Mantenimiento'
+    depto.options[1].value = 'MEN'
+    depto.options[1].text = 'Mensajería'
+    depto.options[2].value = 'SER'
+    depto.options[2].text = 'Servicios Generales'
+  }
+  calcularNomina()
 }
